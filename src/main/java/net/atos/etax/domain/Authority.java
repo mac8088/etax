f4c31515028ff426b9b4f1829d7c3f57eac4edab
Ma Chun <chun.ma@qq.com>
2019-10-09 17:59:47 +0800

    update for TS08_PROFILE

--- src/main/java/net/atos/etax/domain/Authority.java
+++ src/main/java/net/atos/etax/domain/Authority.java
@@ -2,6 +2,9 @@ package net.atos.etax.domain;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -9,11 +12,12 @@ import javax.persistence.Column;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.time.ZonedDateTime;
 import java.util.Objects;
 
 /**
- * Authority (Role) Entity
- * An authority (a security role) used by Spring Security.
+ * Authority (Profile) Entity
+ * An authority (a security Profile) used by Spring Security.
  */
 @Entity
 @Table(name = "jhi_authority")
@@ -26,24 +30,44 @@ public class Authority implements Serializable {
 	@Size(max = 50)
 	@Id
 	@Column(length = 50)
-	/** name of the role. */
+	/** name of the profile. */
 	private String name;
 
 	@Size(max = 200)
 	@Column(length = 200)
-	/** Description of the role. */
-	private String desc;
+	/** Description of the profile. */
+	private String description;
 
 	@NotNull
 	@Size(max = 40)
 	@Column(length = 40)
 	/** STD[MODULE], name of the module this application belongs to. */
-	private String module;
+	private String cstd_module;
+	
+    /**
+     * Effective date
+     */
+    @NotNull
+    @ApiModelProperty(value = "Effective date", required = true)
+    @Column(name = "effective_date", nullable = false)
+    private ZonedDateTime effectiveDate;
+
+    /**
+     * Expired date
+     */
+    @ApiModelProperty(value = "Expiry date")
+    @Column(name = "expiry_date")
+    private ZonedDateTime expiryDate;
+	
+	@Size(max = 40)
+	@Column(length = 40)
+	/** STD[STATUS], the status of the current profile. */
+	private String cstd_status;
 
 	@Size(max = 50)
 	@Column(length = 50)
-	/** Application Message ID in FF_MESSAGE. */
-	private String appMessage;
+	/** If defined, ID of the Supervisor Profile. */
+	private String supvr_profile;
 
 	public String getName() {
 		return name;
@@ -53,28 +77,62 @@ public class Authority implements Serializable {
 		this.name = name;
 	}
 
-	public String getDesc() {
-		return desc;
+	public String getDescription() {
+		return description;
 	}
 
-	public void setDesc(String desc) {
-		this.desc = desc;
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
-	public String getModule() {
-		return module;
+	public String getCstd_module() {
+		return cstd_module;
 	}
 
-	public void setModule(String module) {
-		this.module = module;
+	public void setCstd_module(String cstd_module) {
+		this.cstd_module = cstd_module;
 	}
 
-	public String getAppMessage() {
-		return appMessage;
+    public ZonedDateTime getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public Authority effectiveDate(ZonedDateTime effectiveDate) {
+        this.effectiveDate = effectiveDate;
+        return this;
+    }
+
+    public void setEffectiveDate(ZonedDateTime effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    public ZonedDateTime getExpiryDate() {
+        return expiryDate;
+    }
+
+    public Authority expiryDate(ZonedDateTime expiryDate) {
+        this.expiryDate = expiryDate;
+        return this;
+    }
+
+    public void setExpiryDate(ZonedDateTime expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+	public String getCstd_status() {
+		return cstd_status;
 	}
 
-	public void setAppMessage(String appMessage) {
-		this.appMessage = appMessage;
+	public void setCstd_status(String cstd_status) {
+		this.cstd_status = cstd_status;
+	}
+
+	public String getSupvr_profile() {
+		return supvr_profile;
+	}
+
+	public void setSupvr_profile(String supvr_profile) {
+		this.supvr_profile = supvr_profile;
 	}
 
 	@Override
@@ -97,9 +155,9 @@ public class Authority implements Serializable {
 	public String toString() {
         return "Authority{" +
 	        "name='" + name + '\'' +
-	        ", desc='" + desc + '\'' +
-	        ", module='" + module + '\'' +
-	        ", appMessage='" + appMessage + '\'' +
+	        ", desc='" + description + '\'' +
+	        ", module='" + cstd_module + '\'' +
+	        ", superior='" + supvr_profile + '\'' +
         "}";
 	}
 }
