1feb657da42fecdc49957abd62e05cd45a37d6a0
Ma Chun <chun.ma@qq.com>
2019-10-11 22:11:41 +0800

    testing with group

--- src/main/java/net/atos/bpm/UserCreatorCommandLineRunner.java
+++ src/main/java/net/atos/bpm/UserCreatorCommandLineRunner.java
@@ -3,6 +3,7 @@ package net.atos.bpm;
 import org.flowable.idm.api.IdmIdentityService;
 import org.flowable.idm.api.Privilege;
 import org.flowable.idm.api.User;
+import org.flowable.idm.api.Group;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -24,6 +25,16 @@ public class UserCreatorCommandLineRunner implements CommandLineRunner {
 		createUserIfNotExists("anonymoususer", "123456");
 		createUserIfNotExists("admin", "admin");
 		createUserIfNotExists("user", "user");
+		createUserIfNotExists("sysadmin", "123456");
+		createUserIfNotExists("syscontr", "123456");
+
+		createGroupIfNotExist("ADM001", "System Admin");
+		createGroupIfNotExist("ADM002", "System Supervisor");
+		createGroupIfNotExist("TECHSPT", "Technical support");
+
+		idmIdentityService.createMembership("sysadmin", "ADM001");
+		idmIdentityService.createMembership("sysadmin", "TECHSPT");
+		idmIdentityService.createMembership("syscontr", "ADM002");
 
 		if (idmIdentityService.createPrivilegeQuery().privilegeName("ROLE_USER").count() == 0) {
 			Privilege restPrivilege = idmIdentityService.createPrivilege("ROLE_USER");
@@ -36,6 +47,7 @@ public class UserCreatorCommandLineRunner implements CommandLineRunner {
 			idmIdentityService.addUserPrivilegeMapping(restPrivilege.getId(), "admin");
 			idmIdentityService.addUserPrivilegeMapping(restPrivilege.getId(), "system");
 		}
+
 	}
 
 	protected void createUserIfNotExists(String username, String password) {
@@ -44,5 +56,41 @@ public class UserCreatorCommandLineRunner implements CommandLineRunner {
 			user.setPassword(password);
 			idmIdentityService.saveUser(user);
 		}
+	}
+
+	protected void createGroupIfNotExist(String groupId, String groupName) {
+		if (idmIdentityService.createGroupQuery().groupId(groupId).count() == 0) {
+			Group group = idmIdentityService.newGroup(groupId);
+			group.setName(groupName);
+			idmIdentityService.saveGroup(group);
+		}
+	}
+
+	// 创建用户方法
+	static User creatUser(IdmIdentityService identityService, String id, String first, String last, String email,
+			String passwd) {
+		// 使用newUser方法创建User实例
+		User user = identityService.newUser(id);
+		// 设置用户的各个属性
+		user.setFirstName(first);
+		user.setLastName(last);
+		user.setEmail(email);
+		user.setPassword(passwd);
+		
+		// 使用saveUser方法保存用户
+		identityService.saveUser(user);
+		return identityService.createUserQuery().userId(id).singleResult();
+	}
+
+	// 将用户组数据保存到数据库中
+	static Group createGroup(IdmIdentityService identityService, String id, String name, String type) {
+		// 调用newGroup方法创建Group实例
+		Group group = identityService.newGroup(id);
+		group.setName(name);
+		group.setType(type);
+		
+		// 使用saveGroup方法保存用户
+		identityService.saveGroup(group);
+		return identityService.createGroupQuery().groupId(id).singleResult();
 	}
 }
