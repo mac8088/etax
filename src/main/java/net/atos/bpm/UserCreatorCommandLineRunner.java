1dd3cb2189527acf674fa7289a0921454a14fb87
Ma Chun <chun.ma@qq.com>
2019-10-15 20:59:24 +0800

    ORG 映射集成： 1. 使用视图 2. 使用数据同步 3. 使用自定义的SessionFactory，查询自己的用户和组
    使用此类，为adm中工作流的集成，灌入简单的测试数据。

--- src/main/java/net/atos/bpm/UserCreatorCommandLineRunner.java
+++ src/main/java/net/atos/bpm/UserCreatorCommandLineRunner.java
@@ -1,9 +1,10 @@
 package net.atos.bpm;
 
+import static net.atos.bpm.util.FlowableUtil.createGroupIfNotExist;
+import static net.atos.bpm.util.FlowableUtil.createUserIfNotExists;
+
 import org.flowable.idm.api.IdmIdentityService;
 import org.flowable.idm.api.Privilege;
-import org.flowable.idm.api.User;
-import org.flowable.idm.api.Group;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -21,76 +22,64 @@ public class UserCreatorCommandLineRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-		createUserIfNotExists("system", "system");
-		createUserIfNotExists("anonymoususer", "123456");
-		createUserIfNotExists("admin", "admin");
-		createUserIfNotExists("user", "user");
-		createUserIfNotExists("sysadmin", "123456");
-		createUserIfNotExists("syscontr", "123456");
 
-		createGroupIfNotExist("ADM001", "System Admin");
-		createGroupIfNotExist("ADM002", "System Supervisor");
-		createGroupIfNotExist("TECHSPT", "Technical support");
+		// some JHipster built-in users
+		createUserIfNotExists(idmIdentityService, "system", "system");
+		createUserIfNotExists(idmIdentityService, "anonymoususer", "123456");
+		createUserIfNotExists(idmIdentityService, "admin", "admin");
+		createUserIfNotExists(idmIdentityService, "user", "user");
 
-		idmIdentityService.createMembership("sysadmin", "ADM001");
-		idmIdentityService.createMembership("sysadmin", "TECHSPT");
-		idmIdentityService.createMembership("syscontr", "ADM002");
+		// some eTax users
+		createUserIfNotExists(idmIdentityService, "sysadmin", "123456");
+		createUserIfNotExists(idmIdentityService, "syscontr", "123456");
 
-		if (idmIdentityService.createPrivilegeQuery().privilegeName("ROLE_USER").count() == 0) {
-			Privilege restPrivilege = idmIdentityService.createPrivilege("ROLE_USER");
-			idmIdentityService.addUserPrivilegeMapping(restPrivilege.getId(), "user");
-			idmIdentityService.addUserPrivilegeMapping(restPrivilege.getId(), "system");
-		}
-
-		if (idmIdentityService.createPrivilegeQuery().privilegeName("ROLE_ADMIN").count() == 0) {
-			Privilege restPrivilege = idmIdentityService.createPrivilege("ROLE_ADMIN");
-			idmIdentityService.addUserPrivilegeMapping(restPrivilege.getId(), "admin");
-			idmIdentityService.addUserPrivilegeMapping(restPrivilege.getId(), "system");
-		}
-
-	}
-
-	protected void createUserIfNotExists(String username, String password) {
-		if (idmIdentityService.createUserQuery().userId(username).count() == 0) {
-			User user = idmIdentityService.newUser(username);
-			user.setPassword(password);
-			idmIdentityService.saveUser(user);
-		}
-	}
-
-	protected void createGroupIfNotExist(String groupId, String groupName) {
-		if (idmIdentityService.createGroupQuery().groupId(groupId).count() == 0) {
-			Group group = idmIdentityService.newGroup(groupId);
-			group.setName(groupName);
-			idmIdentityService.saveGroup(group);
-		}
-	}
-
-	// 创建用户方法
-	static User creatUser(IdmIdentityService identityService, String id, String first, String last, String email,
-			String passwd) {
-		// 使用newUser方法创建User实例
-		User user = identityService.newUser(id);
-		// 设置用户的各个属性
-		user.setFirstName(first);
-		user.setLastName(last);
-		user.setEmail(email);
-		user.setPassword(passwd);
+		// some eTax user-groups
+		createGroupIfNotExist(idmIdentityService, "ADM001", "System Admin");
+		createGroupIfNotExist(idmIdentityService, "ADM002", "System Supervisor");
+		createGroupIfNotExist(idmIdentityService, "TECHSPT", "Technical support");
 		
-		// 使用saveUser方法保存用户
-		identityService.saveUser(user);
-		return identityService.createUserQuery().userId(id).singleResult();
+		// Maintain membership between users and groups
+		if (idmIdentityService.createUserQuery().memberOfGroup("ADM001").count() == 0) {
+			idmIdentityService.createMembership("sysadmin", "ADM001");
+		}
+
+		if (idmIdentityService.createUserQuery().memberOfGroup("TECHSPT").count() == 0) {
+			idmIdentityService.createMembership("sysadmin", "TECHSPT");
+		}
+
+		if (idmIdentityService.createUserQuery().memberOfGroup("ADM002").count() == 0) {
+			idmIdentityService.createMembership("syscontr", "ADM002");
+		}
+
+		// setup privilege with groups: Maintain Office
+		if (idmIdentityService.createPrivilegeQuery().privilegeName("PRIV_ADM001").count() == 0) {
+			Privilege restPrivilege = idmIdentityService.createPrivilege("PRIV_ADM001");
+			idmIdentityService.addGroupPrivilegeMapping(restPrivilege.getId(), "ADM001");
+		}
+
+		// setup privilege with groups: Maintain User
+		if (idmIdentityService.createPrivilegeQuery().privilegeName("PRIV_ADM002").count() == 0) {
+			Privilege restPrivilege = idmIdentityService.createPrivilege("PRIV_ADM002");
+			idmIdentityService.addGroupPrivilegeMapping(restPrivilege.getId(), "ADM001");
+		}
+
+		// setup privilege with groups: Approve User Profile
+		if (idmIdentityService.createPrivilegeQuery().privilegeName("PRIV_ADM003").count() == 0) {
+			Privilege restPrivilege = idmIdentityService.createPrivilege("PRIV_ADM003");
+			idmIdentityService.addGroupPrivilegeMapping(restPrivilege.getId(), "ADM002");
+		}
+
+		// setup privilege with groups: Maintain Public Holidays
+		if (idmIdentityService.createPrivilegeQuery().privilegeName("PRIV_ADM004").count() == 0) {
+			Privilege restPrivilege = idmIdentityService.createPrivilege("PRIV_ADM004");
+			idmIdentityService.addGroupPrivilegeMapping(restPrivilege.getId(), "ADM001");
+		}
+
+		// setup privilege with groups: Maintain Exchange Rates
+		if (idmIdentityService.createPrivilegeQuery().privilegeName("PRIV_ADM005").count() == 0) {
+			Privilege restPrivilege = idmIdentityService.createPrivilege("PRIV_ADM005");
+			idmIdentityService.addGroupPrivilegeMapping(restPrivilege.getId(), "ADM001");
+		}
 	}
 
-	// 将用户组数据保存到数据库中
-	static Group createGroup(IdmIdentityService identityService, String id, String name, String type) {
-		// 调用newGroup方法创建Group实例
-		Group group = identityService.newGroup(id);
-		group.setName(name);
-		group.setType(type);
-		
-		// 使用saveGroup方法保存用户
-		identityService.saveGroup(group);
-		return identityService.createGroupQuery().groupId(id).singleResult();
-	}
 }
